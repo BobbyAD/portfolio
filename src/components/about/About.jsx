e9fad9619759e2c996a8433bf6b92fa760d2d835
Robert Driskell <driskell.ba@gmail.com>
2019-12-02 20:04:56 -0600

    improved About

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
                 About Me
             </Typography>
             <Typography variant="body1">
-                I have a Bachelor's of Fine Arts from the University of Texas in Fine Art. After years of working as an artist, I've made the move to software development. I attended Lambda School's Full-Stack Web Development program where I've learned a number of different technologies for both front end and back end development. My experience as an artist sets me apart as a developer who understands both the technical aspects of development, as well as the design and user interface aspects.
+                I have a Bachelor's of Fine Arts from the University of Texas. After a few years of working as an artist, I made the move in to software development in 2018. I attended Lambda School's Full-Stack Web Development program for the past 9 months, where I've learned a number of different technologies from React to Java. My experience as an artist sets me apart as a developer who understands both the technical aspects of development, as well as the design and user interface aspects.
             </Typography>
         </div>
     )
